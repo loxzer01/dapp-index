bb3257fc308859b246e4b40d8445793ff01f0ead
Francisco Banquez <70493879+loxzer01@users.noreply.github.com>
2022-07-18 12:24:27 -0400

    update new landing page

--- js/main.js
+++ js/main.js
@@ -15,6 +15,21 @@ $(document).ready(function(){
             $('.pdo').hide('fade');
         });
     });
-
-
 });
+const calculateinput = document.getElementById('calculateinput');
+const resultC = document.getElementById('result_C');
+const day = [56,112,224];
+let multNumber = 56;
+function changeTime(index){
+    let claseDelete = document.getElementsByClassName('bt-active');
+    let claseAdd = document.getElementsByClassName('calculateClases')[index];
+    claseDelete[0].classList.remove('bt-active');
+    claseAdd.classList.add('bt-active');
+    multNumber = day[index];
+}
+calculateinput.addEventListener('input',function(e){
+    let value = (e.target.value).replace(/[^0-9.]/g, '');
+    let free = (value * 22 / 10000) * 28/100;
+    let result = (value * 22 / 10000)-free;
+    resultC.innerHTML = (result * multNumber).toFixed(2);
+})
